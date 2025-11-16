349956518df2c27abf0b7e370e16e8b46d09c3af
nedled <matheusmned@gmail.com>
2025-11-15 23:25:16 -0300

    correcao da pagina do vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //Aqui e o inicio
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },//Aqui e o final
 })
