3960f17c2afbc12c70e2f416b3298e6b89b155d8
nedled <matheusmned@gmail.com>
2025-11-18 21:29:59 -0300

    correcao de imports react

--- src/pages/dashboard/new/New.tsx
+++ src/pages/dashboard/new/New.tsx
@@ -1,48 +1,6 @@
-import { FiTrash, FiUpload } from "react-icons/fi";
-import Container from "../../../components/container/Container";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Input from "../../../components/input/Input";
-import { useContext, useState, type ChangeEvent, useTransition } from "react";
-import { ContextAuth } from "../../../context/AuthContext";
-import { v4 as uuidV4 } from "uuid";
-import { storage, db } from "../../../services/firebase";
-import {
-  getDownloadURL,
-  deleteObject,
-  ref,
-  uploadBytes,
-} from "firebase/storage";
-import { addDoc, collection } from "firebase/firestore";
-import toast from "react-hot-toast";
 import CarForm from "../../../components/carros/CarForm";
+import Container from "../../../components/container/Container";
 
-//validacoes do zod
-const schema = z.object({
-  name: z.string().nonempty("Nome e obrigatorio"),
-  model: z.string().nonempty("Modelo e obrigatorio"),
-  ano: z.string().nonempty("Ano e obrigatorio"),
-  km: z.string().nonempty("Km e obrigatorio"),
-  preco: z.string().nonempty("Preco e obrigatorio"),
-  city: z.string().nonempty("Cidade e obrigatorio"),
-  whatsapp: z
-    .string()
-    .min(1, "Whatsapp e obrigatorio")
-    .refine((value) => /^(\d{11,12})$/.test(value), {
-      message: "Numero de telefone invalido",
-    }),
-  descricao: z.string().nonempty("Descricao e obrigatorio"),
-});
-//tipagen zod
-type FormData = z.infer<typeof schema>;
-//tipagem imagem carro
-interface ImageCar {
-  name: string;
-  uid: string | undefined;
-  previewUrl: string;
-  url: string;
-}
 export default function New() {
   return (
     <>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,75 +1,7 @@
-import { useEffect, useState } from "react";
-import Container from "../../components/container/Container";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
-import { db } from "../../services/firebase";
-import { Link } from "react-router-dom";
-import type { GetCarros } from "../../types/carrosType";
-import { BiSearch } from "react-icons/bi";
 import CarrosComponent from "../../components/carros/CarrosComponent";
+import Container from "../../components/container/Container";
 
 export default function Home() {
-  const [carros, setCarros] = useState<GetCarros[]>([]);
-  const [loadingCarros, setLoadingCarros] = useState<string[]>([]);
-  const [searchCar, setSearchCar] = useState("");
-  useEffect(() => {
-    rederizarCarros();
-  }, []);
-  function rederizarCarros() {
-    const colleCtionCarros = collection(db, "carros");
-    const queryCarros = query(colleCtionCarros, orderBy("date", "desc"));
-    getDocs(queryCarros).then((resp) => {
-      let carrosEach = [] as GetCarros[];
-      resp.forEach((car) => {
-        carrosEach.push({
-          id: car.id,
-          name: car.data().name,
-          model: car.data().model,
-          ano: car.data().ano,
-          km: car.data().km,
-          preco: car.data().preco,
-          city: car.data().city,
-          whatsapp: car.data().whatsapp,
-          images: car.data().images,
-          date: car.data().date,
-          uid: car.data().uid,
-        });
-      });
-      setCarros(carrosEach);
-    });
-  }
-  async function buscarCarros() {
-    if (!searchCar) {
-      rederizarCarros();
-      return;
-    }
-    const queryCar = query(
-      collection(db, "carros"),
-      where("name", ">=", searchCar.toUpperCase()),
-      where("name", "<=", searchCar.toUpperCase() + "\uf8ff")
-    );
-    await getDocs(queryCar).then((cars) => {
-      let carrosEach = [] as GetCarros[];
-      cars.forEach((car) => {
-        carrosEach.push({
-          id: car.id,
-          name: car.data().name,
-          model: car.data().model,
-          ano: car.data().ano,
-          km: car.data().km,
-          preco: car.data().preco,
-          city: car.data().city,
-          whatsapp: car.data().whatsapp,
-          images: car.data().images,
-          date: car.data().date,
-          uid: car.data().uid,
-        });
-      });
-      setCarros(carrosEach);
-    });
-  }
-  function onloadCar(id: string) {
-    setLoadingCarros((val) => [...val, id]);
-  }
   return (
     <>
       <Container>
